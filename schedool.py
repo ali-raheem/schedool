77f483a1d0a507dacd1902bd7fa34e386fc89d3d
Ali Raheem <ali-windows@shoryuken.me>
2022-08-21 21:57:12 +0100

    fix algo minor

--- schedool.py
+++ schedool.py
@@ -3,28 +3,32 @@
 from random import shuffle
 from functools import reduce
 
-picks = [{"n": "Ali", "j": [0, 3, 0], "c": [7, 1, 0]},
-         {"n": "Claire", "j": [1, 1, 7], "c": [5, 1, 2]},
-         {"n": "Dale", "j": [2, 3, 6], "c": [3, 1, 2]},
-         {"n": "Eric", "j": [0, 3, 6], "c": [7, 1, 2]},
-         {"n": "Fred", "j": [0, 1, 4], "c": [4, 1, 2]},
-         {"n": "George", "j": [5, 6, 3], "c": [7, 4, 1]},
-         {"n": "Hollie", "j": [4, 5, 6], "c": [7, 3, 2]},
-         {"n": "India", "j": [1, 6, 7], "c": [4, 7, 1]},
-         {"n": "Jack", "j": [0, 2, 5], "c": [3, 7, 4]}]
+picks = [
+    {"n": "NAME 0","j":[11,10,12],"c":[3,3,3]},
+    {"n": "NAME 1", "j": [4,4,4], "c": [4,4,4]},
+    {"n": "NAME 2", "j": [None ,None,None], "c": [7,6,8]},
+    {"n": "NAME 3","j":[3,6,8],"c":[3,6,7]},
+    {"n": "NAME 4","j":[1,2,3],"c":[5,6,7]},
+    {"n": "NAME 5", "j":[9,6,3],"c":[5,6,9]},
+    {"n": "NAME 6","j":[6,5,7],"c":[None,None,None]},
+    {"n": "NAME 7","j":[5,3,2],"c":[8,9,7]},
+    {"n": "NAME 8","j":[1,3,5],"c":[3,5,6]},
+    {"n": "NAME 9","j":[None,None,None],"c":[None,None,None]}
+]
 
-journal_dates = [{'n': e['n'], 'd': e['j']} for e in picks]
-case_dates = [{'n': e['n'], 'd': e['c']} for e in picks]
-journal_prealloc = 1
-case_prealloc = 0
+journal_dates = [{'n': e['n'], 'd': [d - 1 if d != None else None for d in e['j']]} for e in picks]
+case_dates = [{'n': e['n'], 'd': [d - 1 if d!= None else None for d in e['c']]} for e in picks]
+journal_prealloc = ['LD Conference']
+case_prealloc = ['LD Conference', 'Amina Rawat']
 
-PICKED_DATE_WEIGHT = 6
-RANDOM_DATE_PENALTY = 6
+PICKED_DATE_WEIGHT = 10
+RANDOM_DATE_PENALTY = 10
 MAX_PICKS = 3
 MIN_SCORE = -1000
 SPACING_WEIGHT = 1
 RUNS = 100000
 NUM_PEOPLE = len(picks)
+NUM_DATES = 12
 
 def rand_rank(group, pa):
     """
@@ -83,10 +87,11 @@ def allocate(group):
     """
     randoms = 0
     picks = [0] * MAX_PICKS
-    allocation = [None] * NUM_PEOPLE
+    allocation = [None] * NUM_DATES
     allocation_score = 0
     if group == None:
         group = []
+    random_allocations = []
     for p in group:
         allocated = False
         for index, d in enumerate(p['d']):
@@ -99,13 +104,17 @@ def allocate(group):
                 picks[index] += 1
                 break
         if allocated == False:
-            free_slot = allocation.index(None)
-            allocation[free_slot] = p['n']
+#            free_slot = allocation.index(None)
+#            allocation[free_slot] = p['n']
+            random_allocations.append(p['n'])
             allocation_score -= RANDOM_DATE_PENALTY
             randoms += 1
+    for name in random_allocations:
+        free_slot = allocation.index(None)
+        allocation[free_slot] = name
     return (allocation, allocation_score, picks, randoms)
 
-best_allocation = [None] * NUM_PEOPLE
+best_allocation = [None] * NUM_DATES
 best_allocation_score = MIN_SCORE
 
 allocation = {'score': MIN_SCORE, 'pre-allocated': journal_prealloc + case_prealloc}
@@ -139,3 +148,9 @@ for _ in range(RUNS):
         allocation['c_picks'] = case_picks
         allocation['score'] = score
 print(f"After {RUNS} runs best was {allocation}.")
+print("Journal Club Order")
+for n in allocation['j']:
+    print(n)
+print("Case Presentation Order")
+for n in allocation['c']:
+    print(n)
